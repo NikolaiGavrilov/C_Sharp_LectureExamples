d0d95fb32fd4f20ad9e3d783b0fa8d9c1032a2d6
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-12 21:58:38 +0300

    Completed Example010

---
+++ Example010_MethodArray/Program.cs
@@ -0,0 +1,13 @@
+﻿int[] array = {1, 21, 23, 4, 54, 65, 43, 76, 88, 43};
+int n = array.Length;
+int find = 43;
+int index = 0;
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+   index++;    
+}
