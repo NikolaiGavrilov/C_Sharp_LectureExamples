0ed15e245f04488ea701a3e86004260b9ad051c3
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-12 22:38:21 +0300

    Added Example007

---
+++ Example007_Magic/Program.cs
@@ -0,0 +1,47 @@
+﻿Console.Clear(); //очистка консоли
+//Console.SetCursorPosition(10, 4); //Курсор с отступами слева и сверху
+//Console.WriteLine("+"); // Указываем, чем будем рисовать - плюсиками
+
+//задаем точки треугольника
+int xa = 23, ya = 1, 
+    xb = 1, yb = 15,
+    xc = 46, yc = 15;
+
+Console.SetCursorPosition(xa, ya);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xb, yb);
+Console.WriteLine("+");
+
+Console.SetCursorPosition(xc, yc);
+Console.WriteLine("+");
+
+int x = xa, y = xb;
+int count = 0;
+while (count < 10000)
+{
+    int what = new Random().Next(0, 3); // [0, 3) 0 1 2
+    if (what == 0)
+    {
+        x = (x+xa)/2;
+        y = (y+ya)/2;
+    }
+
+    if (what == 1)
+    {
+        x = (x+xb)/2;
+        y = (y+yb)/2;
+    }
+
+    if (what == 2)
+    {
+        x = (x+xc)/2;
+        y = (y+yc)/2;
+    }
+
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine("+");
+    count++;
+}
+
+
