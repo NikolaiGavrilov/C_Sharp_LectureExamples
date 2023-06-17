6064d53b35522ab477bc6d24bdc35fcff6466f9c
Nikolay Gavrilov <nikolaisibir@mail.ru>
2023-06-17 13:33:32 +0300

    Added Example004 Div

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numberA1 = 12;
+int numberB1 = 5;
+Console.Write($"Результат деления {numberA1} на {numberB1} с целочисленным интаджером: ");
+Console.WriteLine(numberA1 / numberB1);
+
+double numberA2 = 12;
+int numberB2 = 5;
+Console.Write($"Результат деления {numberA2} на {numberB2} как вещественных чисел: ");
+Console.WriteLine(numberA2 / numberB2);
